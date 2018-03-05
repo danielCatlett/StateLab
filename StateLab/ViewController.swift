ae3fb0417547288f7352740632e096eda22bf140
danielCatlett <daniel.catlett42@gmail.com>
2018-03-04 20:39:37 -0600

    Second commit

--- StateLab/ViewController.swift
+++ StateLab/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController
     private var label: UILabel!
     private var smiley: UIImage!
     private var smileyView: UIImageView!
+    private var segmentedControl:UISegmentedControl!
+    private var index = 0
     private var animate = false
     
     override func viewDidLoad()
@@ -24,23 +26,80 @@ class ViewController: UIViewController
         label.textAlignment = NSTextAlignment.center
         label.backgroundColor = UIColor.clear
         
-        // smiley.png is 84 x 84
-        let smileyFrame = CGRect(x: bounds.midX - 42, y: bounds.midY/2 - 42, width: 84, height: 84)
-        smileyView = UIImageView(frame:smileyFrame)
-        smileyView.contentMode = UIViewContentMode.center
-        let smileyPath = Bundle.main.path(forResource: "smiley", ofType: "png")!
-        smiley = UIImage(contentsOfFile: smileyPath)
-        smileyView.image = smiley
-        view.addSubview(smileyView)
+        //smiley.png is 84 x 84
+        //Tried to get this to work, but kept failing. And the book instructions were less than clear
+//        let smileyFrame = CGRect(x: bounds.midX - 42, y: bounds.midY/2 - 42, width: 84, height: 84)
+//        smileyView = UIImageView(frame:smileyFrame)
+//        smileyView.contentMode = UIViewContentMode.center
+//        let smileyPath = Bundle.main.path(forResource: "smiley", ofType: "png")!
+//        smiley = UIImage(contentsOfFile: smileyPath)
+//        smileyView.image = smiley
+        segmentedControl = UISegmentedControl(items: ["One", "Two", "Three", "Four"])
+        segmentedControl.frame = CGRect(x: bounds.origin.x + 20, y: 50, width: bounds.size.width - 40, height: 30)
+        segmentedControl.addTarget(self, action: #selector(ViewController.selectionChanged(_:)),
+        for: UIControlEvents.valueChanged)
+        view.addSubview(segmentedControl)
+//        view.addSubview(smileyView)
         
         view.addSubview(label)
+        index = UserDefaults.standard.integer(forKey: "index")
+        segmentedControl.selectedSegmentIndex = index
         
         let center = NotificationCenter.default
         center.addObserver(self, selector: #selector(ViewController.applicationWillResignActive),
         name: Notification.Name.UIApplicationWillResignActive, object: nil)
         center.addObserver(self, selector: #selector(ViewController.applicationDidBecomeActive),
         name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
-    } 
+        center.addObserver(self, selector: #selector(ViewController.applicationDidEnterBackground),
+        name: NSNotification.Name.UIApplicationDidEnterBackground, object: nil)
+        center.addObserver(self, selector: #selector(ViewController.applicationWillEnterForeground),
+        name: NSNotification.Name.UIApplicationWillEnterForeground, object: nil)
+    }
+    
+    @objc func applicationDidEnterBackground()
+    {
+        print("VC: \(#function)")
+        UserDefaults.standard.set(self.index, forKey:"index")
+        
+        let app = UIApplication.shared
+        var taskId = UIBackgroundTaskInvalid
+        let id = app.beginBackgroundTask()
+        {
+            print("Background task ran out of time and was terminated.")
+            app.endBackgroundTask(taskId)
+        }
+        taskId = id
+        if taskId == UIBackgroundTaskInvalid
+        {
+            print("Failed to start background task!")
+            return
+        }
+        
+        DispatchQueue.global(qos: .default).async
+        {
+            print("Starting background task with " + "\(app.backgroundTimeRemaining) seconds remaining")
+//            self.smiley = nil;
+//            self.smileyView.image = nil;
+            
+            Thread.sleep(forTimeInterval: 25)
+            
+            print("Finishing background task with " + "\(app.backgroundTimeRemaining) seconds remaining")
+            app.endBackgroundTask(taskId)
+        }
+    }
+    
+    @objc func applicationWillEnterForeground()
+    {
+        print("VC: \(#function)")
+//        let smileyPath = Bundle.main.path(forResource: "smiley", ofType:"png")!
+//        smiley = UIImage(contentsOfFile: smileyPath)
+//        smileyView.image = smiley
+    }
+    
+    @objc func selectionChanged(_ sender:UISegmentedControl)
+    {
+        index = segmentedControl.selectedSegmentIndex;
+    }
     
     func rotateLabelDown()
     {
