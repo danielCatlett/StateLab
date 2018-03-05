67652396938154307addfcd812465eeecbebdf8f
danielCatlett <daniel.catlett42@gmail.com>
2018-03-04 19:55:32 -0600

    Initial commit

--- StateLab/AppDelegate.swift
+++ StateLab/AppDelegate.swift
@@ -1,46 +1,44 @@
-//
 //  AppDelegate.swift
 //  StateLab
 //
 //  Created by Daniel Catlett on 3/4/18.
 //  Copyright © 2018 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+    {
+        print(#function)
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func applicationWillResignActive(_ application: UIApplication)
+    {
+        print(#function)
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func applicationDidEnterBackground(_ application: UIApplication)
+    {
+        print(#function)
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func applicationWillEnterForeground(_ application: UIApplication)
+    {
+        print(#function)
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    func applicationDidBecomeActive(_ application: UIApplication)
+    {
+        print(#function)
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func applicationWillTerminate(_ application: UIApplication)
+    {
+        print(#function)
     }
-
-
 }
-
--- StateLab/ViewController.swift
+++ StateLab/ViewController.swift
@@ -1,25 +1,86 @@
-//
 //  ViewController.swift
 //  StateLab
 //
 //  Created by Daniel Catlett on 3/4/18.
 //  Copyright © 2018 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    private var label: UILabel!
+    private var smiley: UIImage!
+    private var smileyView: UIImageView!
+    private var animate = false
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let bounds = view.bounds
+        let labelFrame = CGRect(origin: CGPoint(x: bounds.origin.x, y: bounds.midY - 50) , size: CGSize(width:  bounds.size.width, height: 100))
+        label = UILabel(frame: labelFrame)
+        label.font = UIFont(name: "Helvetica", size:70)
+        label.text = "Bazinga!"
+        label.textAlignment = NSTextAlignment.center
+        label.backgroundColor = UIColor.clear
+        
+        // smiley.png is 84 x 84
+        let smileyFrame = CGRect(x: bounds.midX - 42, y: bounds.midY/2 - 42, width: 84, height: 84)
+        smileyView = UIImageView(frame:smileyFrame)
+        smileyView.contentMode = UIViewContentMode.center
+        let smileyPath = Bundle.main.path(forResource: "smiley", ofType: "png")!
+        smiley = UIImage(contentsOfFile: smileyPath)
+        smileyView.image = smiley
+        view.addSubview(smileyView)
+        
+        view.addSubview(label)
+        
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(ViewController.applicationWillResignActive),
+        name: Notification.Name.UIApplicationWillResignActive, object: nil)
+        center.addObserver(self, selector: #selector(ViewController.applicationDidBecomeActive),
+        name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+    } 
+    
+    func rotateLabelDown()
+    {
+        UIView.animate(withDuration: 0.5, animations:
+        {
+            self.label.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        },
+        completion: {(Bool) -> Void in
+            self.rotateLabelUp()
+        })
+    }
+    
+    func rotateLabelUp()
+    {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.label.transform = CGAffineTransform(rotationAngle: 0)
+        },
+        completion: {(Bool) -> Void in
+            if self.animate
+            {
+                self.rotateLabelDown()
+            }
+        })
+    }
+    
+    @objc func applicationWillResignActive()
+    {
+        print("VC: \(#function)")
+        animate = false
+    }
+    
+    @objc func applicationDidBecomeActive()
+    {
+        print("VC: \(#function)")
+        animate = true
+        rotateLabelDown()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
